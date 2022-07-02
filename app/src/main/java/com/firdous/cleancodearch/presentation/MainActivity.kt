8274433fa4dc740e5ad626bc25e56b93c817479d
Firdous <shahanifirdous@gmail.com>
2022-07-03 00:04:37 +0530

    testing

--- app/src/main/java/com/firdous/cleancodearch/presentation/MainActivity.kt
+++ app/src/main/java/com/firdous/cleancodearch/presentation/MainActivity.kt
@@ -7,7 +7,6 @@ import com.firdous.cleancodearch.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        print("ci/cd/eee")
         setContentView(R.layout.activity_main)
     }
 }
